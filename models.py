c689a139c7fdc37eec58c139b19a67aae0f27002
Eamonn Bell <pseudoboarder@gmail.com>
2016-12-04 18:27:10 -0500

    Replace stray tab indent with spaces.

--- models.py
+++ models.py
@@ -62,7 +62,7 @@ class bern_emb_model():
     def plot_params(self, dir_name, labels):
         plot_only = len(labels)
 
-	tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+        tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
         low_dim_embs_alpha2 = tsne.fit_transform(self.alpha.eval()[:plot_only])
         plot_with_labels(low_dim_embs_alpha2[:plot_only], labels[:plot_only], dir_name + '/alpha.eps')
 
